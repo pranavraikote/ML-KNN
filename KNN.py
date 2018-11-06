a8f360504042e25fb327483ae29a80889bd08919
Pranav Raikote <43779710+pranavraikote@users.noreply.github.com>
2018-11-06 16:47:00 +0530

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  6 16:38:32 2018
+
+@author: Pranav
+"""
+#Import modules
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import datasets
+
+#Load the iris dataset
+iris=datasets.load_iris()
+iris_data=iris.data
+iris_labels=iris.target
+
+#Optional, to see the data and labels
+#print(iris_data)
+#print(iris_labels)
+
+#Split the data into training and testing data in 70:30 ratio
+x_train,x_test,y_train,y_test=train_test_split(iris_data,iris_labels,test_size=0.30)
+
+#Create a KNearestClassifier with value of K=5
+classifier=KNeighborsClassifier(n_neighbors=5)
+
+#Fit the data and build the model
+classifier.fit(x_train,y_train)
+
+#Predict using the test data
+y_pred=classifier.predict(x_test)
+
+#Print the Confusion Matrix
+print('Confusion matrix is as follows')
+print(confusion_matrix(y_test,y_pred))
+
+#Print Precision and Recall
+print('Accuracy Metrics')
+print(classification_report(y_test,y_pred))
